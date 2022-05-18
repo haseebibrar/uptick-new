136f41bcd03f5e0f23941bed19384b8ee9b4051f
unknown <lahorewebdesign@gmail.com>
2022-05-19 01:49:59 +0500

    Fixed HR Section Design Issues

--- resources/views/admin/hr.blade.php
+++ resources/views/admin/hr.blade.php
@@ -38,17 +38,16 @@
 
         <div class="btmSection mt-4 px-4 py-4 bgWhite">
             <h2 class="mb-4">Student Statistics</h2>
-            <a class="btn btnGreen" href="/admin/students/add">+ Add Member</a>
             <div class="table-responsive mt-4">
                 <table class="table" id="myDataTable">
                     <thead>
                         <tr>
-                            <th scope="col">ID</th>
-                            <th scope="col">Name</th>
+                            <th data-orderable="false" scope="col">Activity</th>
+                            <th scope="col">Students</th>
                             <th scope="col">Department</th>
                             <th scope="col">Progress</th>
                             <th scope="col">Cancellation</th>
-                            <th scope="col">Actions</th>
+                            <th data-orderable="false" scope="col">Actions</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -59,10 +58,17 @@
                                     $myImage = asset('images/users/'.$student->image);
                             @endphp
                             <tr>
-                                <td class="font-weight-bold"></td>
+                                <td>
+                                    <div class="circleAct"></div>
+                                </td>
                                 <td>{!! ($myImage === "" ? '' : '<img class="rounded-circle" style="height:50px;" src="'.$myImage.'" alt="'.$student->name.'" title="'.$student->name.'" />') !!} {{$student->name}}</td>
                                 <td>{{ $student->deptname }}</td>
-                                <td> 0 </td>
+                                <td>
+                                    <div class="txtCenter">3/6</div>
+                                    <div class="progress">
+                                        <div class="progress-bar" role="progressbar" style="width: 50%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+                                    </div>
+                                </td>
                                 <td>{{ rand(1, 10) }}</td>
                                 <td class="text-nowrap">
                                     <a href="/admin/students/edit/{{$student->id}}" class="btn mr-3 btnGray"><i class="fa fa-edit"></i></a>
@@ -95,10 +101,10 @@
                 [20, 50, 100, 500],
                 [20, 50, 100, 500]
             ],
-            "fnRowCallback": function( nRow, aData, iDisplayIndex, iDisplayIndexFull ) {
-                $('td:eq(0)', nRow).html(iDisplayIndexFull +1);
-            }
+            info: true,
+            lengthChange: false
         });
+        $("#myDataTable_filter").append("<a class='btn btnGrayBg btnSearchBox mr-4 ml-4' href='/admin'>Auto Divide</a><a class='btn btnSearchBox btnGreen' href='/admin/students/add'>+ Add Member</a>")
         $(document).off('click', '.btnDel').on('click', '.btnDel', function(){
             if(confirm("Are you sure you want to delete this?")){
                 myID        = $(this).attr('data-id');
