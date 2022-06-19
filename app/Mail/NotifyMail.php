e5459608d9cf9bc47e679aea00267ed208c5d781
unknown <lahorewebdesign@gmail.com>
2022-06-19 05:44:46 +0500

    Made Changes in ICS File

--- app/Mail/NotifyMail.php
+++ app/Mail/NotifyMail.php
@@ -34,12 +34,13 @@ class NotifyMail extends Mailable
      */
     public function build()
     {
-        // $this->data['icslink'];
+        // dd($this->data['icslink']);
         if(isset($this->data['icslink'])){
-            $data = implode("\r\n", $this->data['icslink']);
+            // $dataMail = implode('\r\n', $this->data['icslink']);
+            // dd($dataMail);
             header("text/calendar");
-            file_put_contents('invite.ics', "\xEF\xBB\xBF".  $data);
-            return $this->view('emails.'.$this->filename)->with('data', $this->data)->attach($this->data['icslink'], ['mime' => 'text/calendar; charset=UTF-8; method=REQUEST']);
+            file_put_contents('invite.ics', '\xEF\xBB\xBF'.  $this->data['icslink']);
+            return $this->view('emails.'.$this->filename)->with('data', $this->data)->attach('invite.ics', ['mime' => 'text/calendar; charset=UTF-8; method=REQUEST']);
         }else
             return $this->view('emails.'.$this->filename)->with('data', $this->data);
     }
