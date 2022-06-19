e9c29cce45f72fa995091c9e71af9e73fc9532d7
unknown <lahorewebdesign@gmail.com>
2022-06-19 21:10:52 +0500

    Made changes in NotifyMail

--- app/Mail/NotifyMail.php
+++ app/Mail/NotifyMail.php
@@ -36,7 +36,7 @@ class NotifyMail extends Mailable
     {
         // $this->data['icslink'];
         if(isset($this->data['icslink']))
-            return $this->view('emails.'.$this->filename)->with('data', $this->data)->attach($this->data['icslink'], ['as' => 'reminder.ics', 'mime' => 'data:text charset=utf8']);
+            return $this->view('emails.'.$this->filename)->with('data', $this->data)->attach($this->data['icslink'], ['as' => 'reminder.ics', 'mime' => 'text/calendar; charset=UTF-8; method=REQUEST']);
         else
             return $this->view('emails.'.$this->filename)->with('data', $this->data);
     }
